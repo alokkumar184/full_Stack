baceb8439bce459b7efabf7337ddd9f3bf14d137
alokkumar184 <129863041+alokkumar184@users.noreply.github.com>
2024-03-24 20:32:59 +0530

    Here Create the server using Http and use fs for Rename and delete the file
    Here Create the server using Http and use fs for Rename and delete the file
    
    /// Day-5 class

--- inbuit/fs.js
+++ inbuit/fs.js
@@ -16,8 +16,39 @@ let fs=require('fs')
 
 // This will add the content in same file
 
-fs.appendFile("mycodefile appcode", `${Math.floor(Math.random()*1000)} This code \n` , function(err){
-    if(err) throw err;
-    console.log(" Add Remdom number")
-})
+// fs.appendFile("mycodefile appcode", `${Math.floor(Math.random()*1000)} This code \n` , function(err){
+//     if(err) throw err;
+//     console.log(" Add Remdom number")
+// })
 
+
+// Read file
+
+        // fs.readFile('mytext.text','utf-8', function(err, data){
+        //      console.log("data", data)
+        // })
+// o.p data <Buffer 54 68 69 73 20 69 73 20 6e 6f 64 65 20 66 73 20 70 61 63 6b 61 67 65>   encoded from of data
+// <buffer-encoded from of data
+
+
+// Delete file  --> Here wrong file so throw the error
+// fs.unlink('test.text', function(err){
+//     if(err) throw err;
+//     console.log("file deleted")
+// })
+
+
+// if file Name is correct then file will be deleted .  here correct file Name so delete the file
+// fs.unlink('mytext.text', function(err){
+//     if(err) throw err;
+//     console.log("file deleted")
+// })
+
+/// ReName the file
+// fs.rename('mycodefile', 'mycodefile.text', function(err){
+//     if(err) throw err;
+//     console.log("file renamed")
+// })
+
+   
+    
---
+++ inbuit/http.js
@@ -0,0 +1,17 @@
+// http use for Create a  server
+
+let http=require('http');
+
+// server need two things req and res ✈️
+//Req- Means what pass to the server (params, query, body)
+// res- what server response (means get from server)
+let server=http.createServer(function(req, res) {
+   res.write("<h1> This  is node server </h1>");
+   res.end();  // close the server
+})
+
+// here tell which port run server
+// port-6777 and avoid 8080 port 
+server.listen(6777)
+
+// in the Brower open localhost:6777
